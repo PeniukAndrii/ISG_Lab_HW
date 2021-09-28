c4c9735af160ec3c39f8d8769d3ed974a17b506d
forsejy <andrij200198@gmail.com>
2021-09-28 23:03:29 +0300

    HM2 add StringBuilder.

--- src/HW2/String/MainTree.java
+++ src/HW2/String/MainTree.java
@@ -1,24 +1,27 @@
 package HW2.String;
 
+import java.util.Arrays;
+
 public class MainTree {
     public static void main(String[] args) {
-        String result = fixString("         onE    oF the biGGest REASONS why    Java is SO poPular is the platform     independence.     PROGRAMS can run on several different types   OF COMPUTER; as long as THE    computer    has a    JaVa RUNtime environment (jRE) installed, a jAVA program CAN run oN it");
-        System.out.println(result);
+        StringBuilder stringBuilder = fixString("         onE    oF the biGGest REASONS why    Java is SO poPular is the platform     independence.     PROGRAMS can run on several different types   OF COMPUTER; as long as THE    computer    has a    JaVa RUNtime environment (jRE) installed, a jAVA program CAN run oN it");
+        System.out.println(stringBuilder);
+
     }
 
-    public static String fixString(String string){
-        String kek = "";
-        String s2 = string
+    public static StringBuilder fixString(String string){
+        StringBuilder stringBuilder = new StringBuilder();
+        String stringNoSpaces = string
                 .replaceAll("\\s+", " ")
                 .toLowerCase()
                 .trim();
-        String[] strings = s2.split(" ");
+        String[] strings = stringNoSpaces.split(" ");
         for (String s : strings) {
-            String s3 = s.substring(0, 1)
-                    .toUpperCase()
-                    + s.substring(1);
-            kek = kek.concat(s3 + " ");
+            String stringDone = s.substring(0, 1).toUpperCase() + s.substring(1);
+            stringBuilder
+                    .append(stringDone)
+                    .append(" ");
         }
-        return kek;
+        return stringBuilder;
     }
 }

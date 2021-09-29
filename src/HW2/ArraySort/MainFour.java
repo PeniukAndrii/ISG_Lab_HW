a1808ebbc1f7c8a68e18b4e30df1f337ca8fd6ea
forsejy <andrij200198@gmail.com>
2021-09-29 11:26:10 +0300

    HM2 fixed array.length BAG.

--- src/HW2/ArraySort/MainFour.java
+++ src/HW2/ArraySort/MainFour.java
@@ -4,16 +4,17 @@ import java.util.Arrays;
 
 public class MainFour {
     public static void main(String[] args) {
-        int[] arrays = {1,3,5,7,9,12};
+        int[] arrays =  {1,3,5,7,9,12};
         int[] arrays2 = {2,3,7,7,8,9,10,15,21,35};
         sort(arrays, arrays2);
     }
     public static void sort(int[] array, int[] array2){
         int[] item = new int[array.length + array2.length];
+        int limit = Math.min(array.length, array2.length);
         int k = 0;
         int j = 0;
         for(int i = 0; i < item.length; i++){
-             if(k<array.length){
+             if(k<limit){
                  if(array[k]<=array2[j]){
                      item[i] = array[k];
                      i++;
@@ -25,6 +26,9 @@ public class MainFour {
                      item[i] = array[k];
                  }
                  k++;
+             }
+             else if(array.length > array2.length){
+                 item[i] = array[j];
              }
              else {
                  item[i] = array2[j];

20a0b96b3b2edfaffa4ab27b64d72fb5533d70df
forsejy <andrij200198@gmail.com>
2021-09-30 00:12:05 +0300

    HM2 String Fixed/

--- src/HW2/String/MainTree.java
+++ src/HW2/String/MainTree.java
@@ -1,15 +1,14 @@
 package HW2.String;
 
-import java.util.Arrays;
 
 public class MainTree {
     public static void main(String[] args) {
-        StringBuilder stringBuilder = fixString("         onE    oF the biGGest REASONS why    Java is SO poPular is the platform     independence.     PROGRAMS can run on several different types   OF COMPUTER; as long as THE    computer    has a    JaVa RUNtime environment (jRE) installed, a jAVA program CAN run oN it");
-        System.out.println(stringBuilder);
+        String string = fixString("         onE    oF the biGGest REASONS why    Java is SO poPular is the platform     independence.     PROGRAMS can run on several different types   OF COMPUTER; as long as THE    computer    has a    JaVa RUNtime environment (jRE) installed, a jAVA program CAN run oN it");
+        System.out.println(string);
 
     }
 
-    public static StringBuilder fixString(String string){
+    public static String fixString(String string){
         StringBuilder stringBuilder = new StringBuilder();
         String stringNoSpaces = string
                 .replaceAll("\\s+", " ")
@@ -17,11 +16,8 @@ public class MainTree {
                 .trim();
         String[] strings = stringNoSpaces.split(" ");
         for (String s : strings) {
-            String stringDone = s.substring(0, 1).toUpperCase() + s.substring(1);
-            stringBuilder
-                    .append(stringDone)
-                    .append(" ");
+            stringBuilder.append(s.substring(0, 1).toUpperCase()).append(s.substring(1)).append(" ");
         }
-        return stringBuilder;
+        return stringBuilder.toString();
     }
 }

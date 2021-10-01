0a8eb48e369ca60e33272a7ab3e8ea63246bc72f
forsejy <andrij200198@gmail.com>
2021-10-01 22:15:19 +0300

    Nazarii_HW1/Generic-ArrayList

---
+++ src/HW2/NazariiHW1/task1/Continent.java
@@ -0,0 +1,34 @@
+package HW2.NazariiHW1.task1;
+
+
+public enum Continent {
+    ANTARCTICA("Antarctica"),
+    AMERICA("North America"),
+    EUROPE("Europe"),
+    AFRICA("Africa"),
+    ASIA("Asia"),
+    AUSTRALIA("Australia");
+
+    private String continent;
+    Continent() {
+    }
+
+    Continent(String continent) {
+        this.continent = continent;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    @Override
+    public String toString() {
+        return "Continent{" +
+                "continent='" + continent + '\'' +
+                "} " + super.toString();
+    }
+}
---
+++ src/HW2/NazariiHW1/task1/Main.java
@@ -0,0 +1,30 @@
+package HW2.NazariiHW1.task1;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Country> countries = new ArrayList<>();
+        countries.add(new Country("Moldova","Moldova_Capital",100000,2001, Continent.EUROPE));
+        countries.add(new Country("Indonesia","Indonesia_Capital",20,2021, Continent.AFRICA));
+        countries.add(new Country("China","China_Capital",1411778724,2002, Continent.ASIA));
+        countries.add(new Country("United States","United States_Capital",332457940,1776, Continent.AMERICA));
+        countries.add(new Country("Pakistan","Pakistan_Capital",225200000,2002, Continent.ASIA));
+        countries.add(new Country("Brazil","Brazil_Capital",213750624,2001, Continent.AMERICA));
+        countries.add(new Country("Nigeria","Nigeria_Capital",211401000,1205, Continent.AFRICA));
+        countries.add(new Country("Germany","Germany_Capital",83129285,2005, Continent.EUROPE));
+        countries.add(new Country("France","France_Capital",67486000,2006, Continent.EUROPE));
+        countries.add(new Country("Australia","Australia_Capital",25873870,2006, Continent.AUSTRALIA));
+        countries.add(new Country("Brunei","Brunei_Capital",100000,2008, Continent.ASIA));
+
+        Country.sortedSout(countries);
+        Country.sortedByIndepYear(countries);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input capital:");
+        Country.getCountry(countries, scanner.next());
+        System.out.println("Input continent:");
+        Country.getByContinent(countries, scanner.next());
+
+    }
+}
